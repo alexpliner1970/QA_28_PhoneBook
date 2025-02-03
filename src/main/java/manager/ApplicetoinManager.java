35c7191b9484e59408a102b25fccac4d8f8a96e6
alexpliner1970 <alexpliner3005@gmail.com>
2025-02-03 19:26:54 +0200

    new

---
+++ src/main/java/manager/ApplicetoinManager.java
@@ -0,0 +1,16 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ApplicetoinManager {
+    WebDriver wd;
+
+    public void init(){
+        wd=new ChromeDriver();
+        wd.navigate().to("https://telranedu.web.app/home");
+    }
+    public void stop() {
+        wd.quit();
+    }
+}
